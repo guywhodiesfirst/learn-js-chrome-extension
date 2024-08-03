866279bc0bbf655f8c2b3211f4e6adbf797dbba3
guywhodiesfirst <brizeout@gmail.com>
2024-08-03 11:58:03 +0300

    add saving notes to local storage, delete feature, url check

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-inputBtn = document.getElementById("input-btn")
-inputEl = document.getElementById("input-el")
-listEl = document.getElementById("list-el")
-leads = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const listEl = document.getElementById("list-el")
+const deleteBtn = document.getElementById("delete-btn")
+let notes = []
+
+let localNotes= JSON.parse(localStorage.getItem("myNotes"))
+if(localNotes) {
+    urls = localNotes
+    renderArray(notes)
+}
+
 inputBtn.addEventListener("click", function() {
-    leads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+    if(inputEl.value) {
+        notes.push(inputEl.value)
+        localStorage.setItem("myNotes", JSON.stringify(notes))
+        inputEl.value = ""
+        renderArray(notes)
+    }
 })
 
-function renderLeads() {
-    let listLeads = ""
-    for(let i = 0; i < leads.length; i++) {
-        listLeads += `
+deleteBtn.addEventListener("dblclick", function() {
+    localNotes = []
+    notes = []
+    localStorage.removeItem("myNotes")
+    listEl.innerHTML = ""
+})
+
+function renderArray(array) {
+    let list = ""
+    for(let i = 0; i < array.length; i++) {
+        list += `
             <li>
-                <a target='_blank' href= ${leads[i]}>
-                    ${leads[i]}
-                </a>
+                ${
+                    isCorrectUrl(array[i]) ?
+                    `
+                    <a target='_blank' href= ${array[i]}>
+                        ${array[i]}
+                    </a>
+                    `
+                    : `${array[i]}`
+                }
             </li>
         `
     }
-    listEl.innerHTML = listLeads
+    listEl.innerHTML = list
+}
+
+function isCorrectUrl(str) {
+    try {
+        new URL(str)
+        return true
+    } catch(err) {
+        return false
+    }
 }
